c80a809b758d06e59fc5e185b9a2d14118977a44
toby1a05 <32935616+toby1a05@users.noreply.github.com>
2022-01-25 06:01:31 -0800

    first commit

---
+++ src/com/toby/springMvc/HomeController.java
@@ -0,0 +1,13 @@
+package com.toby.springMvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String mainPage() {
+		return "Main";
+	}
+}
